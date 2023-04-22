2ad80bda0eae06aef66bde196f357e5ce9100152
klaudiabilska <klaudiabilska1@gmail.com>
2023-04-22 17:01:40 +0200

    edit

--- client/script.js
+++ client/script.js
@@ -88,6 +88,33 @@ const handleSubmit = async (e) => {
 
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
+
+  // fetch data from server = bot's response
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if(response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong... Try again"
+
+    alert(err)
+  }
 }
 
 form.addEventListener('submit', handleSubmit)
